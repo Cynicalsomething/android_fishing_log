3267c85916087f4e8f43281806bde84acf1ff7c8
Adam Benjamin <abenjamin@gmail.com>
2011-06-08 15:29:48 -0500

    Removed unnecessary Context import.

--- src/com/Cynical/android/fishingdatabase/AddSpecies.java
+++ src/com/Cynical/android/fishingdatabase/AddSpecies.java
@@ -1,7 +1,6 @@
 package com.Cynical.android.fishingdatabase;
 
 import android.app.Activity;
-import android.content.Context;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
