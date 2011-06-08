fd8aafd9f185087bc6bfaca3a923cc149518a42c
Adam Benjamin <abenjamin@gmail.com>
2011-06-08 16:29:38 -0500

    Removed annoying TODO tags

--- src/com/Cynical/android/fishingdatabase/FDLocationListener.java
+++ src/com/Cynical/android/fishingdatabase/FDLocationListener.java
@@ -8,25 +8,21 @@ public class FDLocationListener implements LocationListener {
 	
 	@Override
 	public void onLocationChanged(Location location) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void onProviderDisabled(String provider) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void onProviderEnabled(String provider) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void onStatusChanged(String provider, int status, Bundle extras) {
-		// TODO Auto-generated method stub
 		
 	}
 
