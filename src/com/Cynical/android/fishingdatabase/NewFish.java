4eb2f6e5b4317bc81cb0503fa795c2efc91a291a
Adam Benjamin <abenjamin@gmail.com>
2011-06-08 16:39:21 -0500

    Switched some stuff around to work more efficiently with the activity lifecycle.

--- src/com/Cynical/android/fishingdatabase/NewFish.java
+++ src/com/Cynical/android/fishingdatabase/NewFish.java
@@ -4,21 +4,70 @@ import android.app.Activity;
 import android.content.Context;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class NewFish extends Activity {
 
+	private LocationManager lm;
+	private FDLocationListener fdll;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.editfish);
-		LocationManager lm = 
-			(LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-		FDLocationListener fdll = new FDLocationListener();
+		lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+		fdll = new FDLocationListener();		
+		
+		Button save = (Button) findViewById(R.id.save_fish_button);
+		Button cancel = (Button) findViewById(R.id.cancel_add_fish_button);
+		
+		save.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				// TODO Pull location and save info to database
+				
+				try {
+					lm.removeUpdates(fdll);
+				} catch (IllegalArgumentException e) {
+					//Do Nothing
+				}
+			}
+		});
+		
+		cancel.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				try {
+					lm.removeUpdates(fdll);
+				} catch (IllegalArgumentException e) {
+					//Do Nothing
+				} finally {
+					NewFish.this.finish();
+				}
+			}
+		});
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
 		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, fdll);
-		
-		
-		
-		lm.removeUpdates(fdll);
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		try {
+			lm.removeUpdates(fdll);
+		} catch (IllegalArgumentException e) {
+			//Do Nothing
+		}
 	}
 	
 }
