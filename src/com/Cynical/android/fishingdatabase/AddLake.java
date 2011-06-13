f8b7eefde7ca9b96663d0181235387f5a5d12af0
Adam Benjamin <abenjamin@gmail.com>
2011-06-13 11:52:00 -0500

    Added a layout and class for adding a lake to the database

---
+++ src/com/Cynical/android/fishingdatabase/AddLake.java
@@ -0,0 +1,15 @@
+package com.Cynical.android.fishingdatabase;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddLake extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+	
+
+}
