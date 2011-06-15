02d7fc58bf379c346e1c383663f597fdab7d4e49
Adam Benjamin <abenjamin@gmail.com>
2011-06-15 14:31:58 -0500

    Switch back intent to call the correct activity

--- src/com/Cynical/android/fishingdatabase/startingUI.java
+++ src/com/Cynical/android/fishingdatabase/startingUI.java
@@ -1,16 +1,11 @@
 package com.Cynical.android.fishingdatabase;
 
-import java.util.Vector;
-
-import com.Cynical.android.webServices.WeatherRetriever;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class startingUI extends Activity {
     /** Called when the activity is first created. */
@@ -30,7 +25,7 @@ public class startingUI extends Activity {
 			
 			@Override
 			public void onClick(View v) {
-				Intent i = new Intent(startingUI.this, ViewFish.class);
+				Intent i = new Intent(startingUI.this, AddLure.class);
 				startActivity(i);
 			}
 		});
